048e79cc24f24ddd8c405538ad30c83451ec9d50
Mathias Geat <mathias.geat@elements.at>
2018-01-11 16:34:19 +0100

    Make setup as quiet as possible

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,15 +5,15 @@ set -eu
 # clone/update pimcore
 if [ -d "tmp/pimcore" ]; then
     echo "Updating pimcore in tmp/pimcore"
-    (cd tmp/pimcore && git reset --hard && git clean -fd && git fetch origin && git checkout master && git pull origin master)
+    (cd tmp/pimcore && git reset -q --hard && git clean -qfd && git fetch -q origin && git checkout -q master && git pull -q origin master)
 else
     echo "Cloning pimcore to tmp/pimcore"
-    git clone https://github.com/pimcore/pimcore.git tmp/pimcore
+    git clone -q https://github.com/pimcore/pimcore.git tmp/pimcore
 fi
 
 if [ ! -f sami.phar ]; then
     # download sami
     echo ""
     echo "Downloading sami from http://get.sensiolabs.org/sami.phar"
-    curl -O http://get.sensiolabs.org/sami.phar
+    curl -s -O http://get.sensiolabs.org/sami.phar
 fi
